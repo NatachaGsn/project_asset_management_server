05f2a737fcf78c76b9d5c5fd18f54ec8369ecee1
Natacha_Gaussin <nat.gaussin@orange.fr>
2026-01-08 17:16:28 +0100

    Traduction of the main page

--- app/Home.py
+++ app/Home.py
@@ -2,8 +2,11 @@ import streamlit as st
 
 st.title("Asset Management Platform")
 
-st.write("Bienvenue dans notre application d'analyse financière.")
-st.write("Utilisez le menu de gauche pour naviguer :")
+st.write("Welcome to our financial analysis application.")
+st.write("Use the menu on the left to navigate:")
 
 st.write("- **Single Asset (Quant A)**")
-st.write("- **Portfolio (Quant B)** – à venir")
+st.write("- **Portfolio (Quant B)**")
+
+st.write("Created by GIRONA Margaux & GAUSSIN Natacha")
+st.write("ESILV IF3")
